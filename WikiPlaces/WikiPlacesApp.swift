a2e375dbc31302a22f97622c09a236767dc46999
Steve Overmars <steveovermars@gmail.com>
2025-11-29 20:08:10 +0100

    Initial Commit

---
+++ WikiPlaces/WikiPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiPlacesApp.swift
+//  WikiPlaces
+//
+//  Created by Steve Overmars on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WikiPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
