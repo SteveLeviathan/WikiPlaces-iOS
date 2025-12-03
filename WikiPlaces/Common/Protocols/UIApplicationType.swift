6c9a1b77517f2bb5546b51981c6fbab08f424103
Steve Overmars <steveovermars@gmail.com>
2025-12-03 21:41:23 +0100

    Added UIApplicationType protocol to make UIApplication.shared injectable and testable

---
+++ WikiPlaces/Common/Protocols/UIApplicationType.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+protocol UIApplicationType: AnyObject {
+    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler completion: (@MainActor @Sendable (Bool) -> Void)?)
+    func canOpenURL(_ url: URL) -> Bool
+}
+
+extension UIApplicationType {
+    public func open(_ url: URL) {
+        open(url, options: [:], completionHandler: nil)
+    }
+}
+
+extension UIApplication: UIApplicationType {}
